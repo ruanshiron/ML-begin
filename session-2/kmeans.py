31af250c9ba9af03d3f07150f529df796d3e68b5
Vinh <theten12@gmail.com>
2020-06-02 00:13:18 +0700

    ~kmeans: random_seed failed

--- session-2/kmeans.py
+++ session-2/kmeans.py
@@ -53,7 +53,14 @@ class Kmeans:
 
   def random_init(self, seed_value):
 
+    return
+
   def compute_similarity(self, member, centroid):
+    distance = np.zeros((member.shape[0], self.num_clusters))
+    for k in range(self.num_clusters):
+      row_norm = numpy.linalg.norm(member - centroid[k, :], axis=1)
+      distance[:, k] = np.square(row_norm)
+    return distance
 
   def select_cluster_for(self, member):
     best_fit_cluster = None
@@ -147,4 +154,73 @@ class Kmeans:
         cj = self._label_count[label] * 1
         H_C += - cj / N * np.log10(cj / N)
       return I_value * 2. / (H_omega + H_C)
+
+def load_data(data_path):
+  def sparse_to_dense(sparse_r_d, vocab_size):
+    r_d = [0.0 for _ in range(vocab_size)]
+    indices_tfidfs = sparse_r_d.split()
+    for index_ifidf in indices_tfidfs:
+      index = int(index_ifidf.split(':')[0])
+      tfidf = float(index_ifidf.split(':')[1])
+      r_d[index] = tfidf
+    return np.array(r_d)
+
+  with open(data_path) as f:
+    d_lines = f.read().splitlines()
+  with open('../datasets/20news-bydate/words_idfs.txt') as f:
+    vocab_size = len(f.read().splitlines())
+
+  _data = []
+  _labels = []
+  _label_count = defaultdict(int)
+  for data_id, d in enumerate(d_lines):
+    features = d.split('<fff>')
+    label, doc_id = int(features[0]), int(features[1])
+    _label_count[label] += 1
+    r_d = sparse_to_dense(sparse_r_d=features[2], vocab_size=vocab_size)
+
+    _data.append(r_d)
+    _labels.append(label)
+  return _data, _labels
+
+def clustering_with_KMeans():
+  data, labels = load_data(data_path='../datasets/20news-bydate/20news-full-tfidf.txt')
+  # use csr_matrix to create a sparse matrix with efficient row slicing
+  from sklearn.cluster import KMeans
+  from scipy.sparse import csr_matrix
+  X = csr_matrix(data)
+  print("=======")
+  kmeans = KMeans(
+    n_clusters=20,
+    init='random',
+    n_init=5,
+    tol=1e-3,
+    random_state=2018
+  ).fit(X)
+  labels = kmeans.labels_
+
+def classifying_with_linear_SVMs():
+  def compute_accuracy(predicted_Y, expected_Y):
+    matches = np.equal(predicted_Y, expected_Y)
+    accuracy = np.sum(matches.astype(float)) / len(expected_Y)
+    return accuracy
+
+  train_X, train_Y = load_data(data_path='../datasets/20news-bydate/20news-train-tfidf.txt')
+  from sklearn.svm import LinearSVC
+  classifier = LinearSVC (
+    C=10.0,
+    tol=0.001,
+    verbose=True
+  )
+  classifier.fit(train_X, train_Y)
+
+  test_X, test_Y = load_data(data_path='../datasets/20news-bydate/20news-test-tfidf.txt')
+  predicted_Y = classifier.predict(test_X)
+  accuracy = compute_accuracy(predicted_Y=predicted_Y, expected_Y=test_Y)
+  print('Accuracy: ', accuracy)
+
+if __name__ == '__main__':
+  # clustering_with_KMeans()
+  classifying_with_linear_SVMs()
+
 
